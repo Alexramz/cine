d69c9e2152cef761444b21f35a33c49bfb7af3d0
Alex Heberardo <alexramz10@gmail.com>
2020-04-13 16:42:30 -0500

    test CD 2

--- src/components/Navbar/Logo.js
+++ src/components/Navbar/Logo.js
@@ -4,14 +4,15 @@ import './App.css';
 
 class Logo extends Component {
 
-  onlogo = () => {
-    this.props.onlogo("react");
+    onlogo = () => {
+      this.props.onlogo("react");
+      }
+
+    render(){
+    return (
+          <img src={logo} alt="logo" width= "50" height="50" onClick={this.onlogo}/>
+    );
     }
 
-  render(){
-  return (
-        <img src={logo} alt="logo" width= "50" height="50" onClick={this.onlogo}/>
-  );
-  }
 }
 export default Logo;
